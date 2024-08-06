dabd4a66671e32fba19c1d47201d48a7c07d55db
prabh <prabh@10.48.48.33>
2024-08-06 16:18:49 -0400

    modify code using enum

--- src/ca/sheridancollege/project/WarPlayer.java
+++ src/ca/sheridancollege/project/WarPlayer.java
@@ -4,10 +4,43 @@
  */
 package ca.sheridancollege.project;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author user
  */
-public class WarPlayer extends Player{
-    
+public class WarPlayer extends Player {
+    private List<WarCard> hand;
+
+    public WarPlayer(String name) {
+        super(name);
+        hand = new ArrayList<>();
+    }
+
+    public List<WarCard> getHand() {
+        return hand;
+    }
+
+    public void setHand(List<WarCard> hand) {
+        this.hand = hand;
+    }
+
+    public void collectCards(WarCard... cards) {
+        System.out.println(this.getName() + " collects cards:");
+        for (WarCard card : cards) {
+            System.out.println(card);
+            hand.add(card);
+        }
+    }
+
+    public boolean hasCards() {
+        return !hand.isEmpty();
+    }
+
+    @Override
+    public void play() {
+        // Implement the player's turn logic here
+    }
 }
